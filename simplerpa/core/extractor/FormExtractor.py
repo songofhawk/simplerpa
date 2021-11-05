547b5f172c091ba345624a4923fe4fceb52340cd
songofhawk <songofhawk@gmail.com>
2021-11-05 23:52:35 +0800

    尝试定义FormExtractor

--- simplerpa/core/extractor/FormExtractor.py
+++ simplerpa/core/extractor/FormExtractor.py
@@ -1,11 +1,18 @@
+from typing import Tuple, List
+
 from core.detection.ImageDetection import ImageDetection
 
 from .Extractor import Extractor
+from ..action.ActionImage import ActionImage
 
 
 class FormExtractor(Extractor):
+    foreground: Tuple[int, int, int] = (0, 0, 0)
+    tolerance: float = 0.01
+    field_def: List[Tuple[str, str]] = None
+
     def __init__(self):
         super().__init__()
 
-    def do(self):
-        parts = self.split_parts()
+    def do_once(self, image):
+        ActionImage.find_content_parts(image, )
