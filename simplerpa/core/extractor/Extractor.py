32f8eccf4dfcf2a3644ea503ad9948a1bb247333
songofhawk <songofhawk@gmail.com>
2021-11-05 23:40:04 +0800

    修复只有start找不到标识的问题

--- simplerpa/core/extractor/Extractor.py
+++ simplerpa/core/extractor/Extractor.py
@@ -30,32 +30,33 @@ class PartSplitter(StateBlockBase):
             end_found_list = self.end.do_detection(image_current)
             if end_found_list is None:
                 print("end template not found in splitter '{}'".format(self.name))
-        else:
-            return None
         if end_found_list is not None:
             end_found_list.sort(key=lambda x: x.rect_on_image.top, reverse=False)
 
-        start_len = len(start_found_list) if start_found_list is not None else 0
-        end_len = len(end_found_list) if end_found_list is not None else 0
-
-        count = min(start_len, end_len)
         pre_top = None
         pre_bottom = None
         parts = []
-        for i in range(count):
-            start_top = start_found_list[i].rect_on_image.top if self.start is not None else None
-            end_bottom = end_found_list[i].rect_on_image.bottom if self.end is not None else None
-            if self.start is not None and self.end is None:
+        if self.start is not None and self.end is None:
+            for start_found in start_found_list:
+                start_top = start_found.rect_on_image.top
                 if pre_top is not None:
                     part = image[pre_top:start_top, :]
                     parts.append(part)
-                    pre_top = start_top
-            elif self.start is None and self.end is not None:
+                pre_top = start_top
+        elif self.start is None and self.end is not None:
+            for end_found in end_found_list:
+                end_bottom = end_found.rect_on_image.bottom
                 if pre_bottom is not None:
                     part = image[pre_bottom:end_bottom, :]
                     parts.append(part)
                 pre_bottom = end_bottom
-            else:
+        else:
+            start_len = len(start_found_list)
+            end_len = len(end_found_list)
+            count = min(start_len, end_len)
+            for i in range(count):
+                start_top = start_found_list[i].rect_on_image.top if self.start is not None else None
+                end_bottom = end_found_list[i].rect_on_image.bottom if self.end is not None else None
                 if start_top < end_bottom:
                     part = image[start_top:end_bottom, :]
                     parts.append(part)
@@ -64,7 +65,6 @@ class PartSplitter(StateBlockBase):
                         part = image[pre_top:end_bottom, :]
                         parts.append(part)
                     pre_top = start_top
-
         return parts
 
 
@@ -84,8 +84,8 @@ class Extractor(StateBlockBase):
 
         if self.part_splitter is not None:
             image_parts = self.part_splitter.split_parts(self.snapshot, image_source)
-            for part in image_parts:
-                ActionImage.log_image('part', part)
+            for index, part in enumerate(image_parts):
+                ActionImage.log_image('part-{}'.format(index), part)
                 self.do_once(part)
         else:
             self.do_once(image_source)
